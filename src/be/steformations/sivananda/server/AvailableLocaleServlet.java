33cd7350b13933180a69a064aeb505642a2ec112
java09 <java09@PRIM2014-17.fapse.priv>
2017-10-11 15:34:55 +0200

    sort avalaible languages

--- src/be/steformations/sivananda/server/AvailableLocaleServlet.java
+++ src/be/steformations/sivananda/server/AvailableLocaleServlet.java
@@ -1,26 +1,39 @@
 package be.steformations.sivananda.server;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Locale;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gwt.dev.util.collect.HashSet;
+
 @SuppressWarnings("serial")
-public class AvaibleLocaleServlet extends HttpServlet {
+public class AvailableLocaleServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		System.out.println("AvaibleLocaleServlet.doGet()");
 		Locale[] locales = Locale.getAvailableLocales();
-		StringBuilder sb = new StringBuilder();
-		for (Locale locale : locales) {
-			sb.append(locale.getLanguage()).append(",");
+		Set<String> set = new HashSet<>();
+		for (Locale l : locales) {
+			if (! l.getLanguage().isEmpty()){
+				set.add(l.getLanguage());
+			}
 		}
+		List<String> list = new ArrayList<String>(set);
+		Collections.sort(list);
+		
+		String s = String.join(",", list);
 		resp.setContentType("text/plain");
-		resp.getWriter().write(sb.substring(0, sb.length()-1));
+		resp.getWriter().write(s);
+		
 	}
 	
 	
